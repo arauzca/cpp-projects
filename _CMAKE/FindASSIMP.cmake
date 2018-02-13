c3d9988c64ca12e75d91a70310c0c37fad3e7c47
Carlos Arauz <arauc1@bfh.ch>
2018-02-13 13:34:29 +0100

    fixed assimp issues

--- _CMAKE/FindASSIMP.cmake
+++ _CMAKE/FindASSIMP.cmake
@@ -1,11 +1,11 @@
 # locate header files and put user specified location at beginning of search
 if(ASSIMP_ROOT_DIR)
-    set(_GLEW_HEADER_SEARCH_DIRS "${ASSIMP_ROOT_DIR}"
+    set(_ASSIMP_HEADER_SEARCH_DIRS "${ASSIMP_ROOT_DIR}"
                                 "${ASSIMP_ROOT_DIR}/include"
                                  ${_ASSIMP_HEADER_SEARCH_DIRS})
 endif(ASSIMP_ROOT_DIR)
 
-find_path(ASSIMP_INCLUDE_DIR "assimp/Importer.hpp" PATHS ${_GLEW_HEADER_SEARCH_DIRS})
+find_path(ASSIMP_INCLUDE_DIR "assimp/Importer.hpp" PATHS ${_ASSIMP_HEADER_SEARCH_DIRS})
 
 if(APPLE)
    message("ASSIMP using mac libs")
@@ -23,7 +23,7 @@ include("FindPackageHandleStandardArgs")
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(ASSIMP DEFAULT_MSG ASSIMP_LIBRARY ASSIMP_LIBRARY_DEBUG ASSIMP_INCLUDE_DIR)
 
 if(ASSIMP_FOUND)
-   set(ASSIMP_INCLUDE_DIRS ${GLEW_INCLUDE_DIR})
-   set(ASSIMP_LIBRARIES ${GLEW_LIBRARY})
-   set(ASSIMP_LIBRARIES_DEBUG ${GLEW_LIBRARY_DEBUG})
+   set(ASSIMP_INCLUDE_DIRS ${ASSIMP_INCLUDE_DIR})
+   set(ASSIMP_LIBRARIES ${ASSIMP_LIBRARY})
+   set(ASSIMP_LIBRARIES_DEBUG ${ASSIMP_LIBRARY_DEBUG})
 endif(ASSIMP_FOUND)
