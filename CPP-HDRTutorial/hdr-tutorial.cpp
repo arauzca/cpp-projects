6b2bda8ccd5842ade2f4ffa8b7ea0ecb4e0938d3
Carlos Arauz <c.arauz@icloud.com>
2018-05-28 19:00:41 +0200

    updated with tonemapping

--- CPP-HDRTutorial/hdr-tutorial.cpp
+++ CPP-HDRTutorial/hdr-tutorial.cpp
@@ -32,13 +32,61 @@ int main()
 	mergeDebevec->process(images, hdrDebevec, times, responseDebevec);
 	// Save HDR image.
 	imwrite("hdrDebevec.hdr", hdrDebevec);
+ 
+    // Tonemap using Drago's method to obtain 8-bit/channel color image
+    Mat ldrDrago;
+    Ptr<TonemapDrago> tonemapDrago = createTonemapDrago(1.0f, 0.7f);
+    tonemapDrago->process(hdrDebevec, ldrDrago);
+    ldrDrago *= 3 * 255;
+    imwrite("ldr-Drago.jpg", ldrDrago);
+    
+    // Tonemap using Durand's method to obtain 8-bit/channel color image
+    Mat ldrDurand;
+    Ptr<TonemapDurand> tonemapDurand = createTonemapDurand(1.5f, 4.0f, 1.0f, 1.0f, 1.0f);
+    tonemapDurand->process(hdrDebevec, ldrDurand);
+    ldrDurand *= 3 * 255;
+    imwrite("ldr-Durand.jpg", ldrDurand);
+    
+    // Tonemap using Reinhard's method to obtain 8-bit/channel color image
+    Mat ldrReinhard;
+    Ptr<TonemapReinhard> tonemapReinhard = createTonemapReinhard(2.2f, 0.0f, 0.0f, 0.0f);
+    tonemapReinhard->process(hdrDebevec, ldrReinhard);
+    ldrReinhard *= 255;
+    imwrite("ldr-Reinhard.jpg", ldrReinhard);
+    
+    // Tonemap using Mantiuk's method to obtain 8-bit/channel color image
+    Mat ldrMantiuk;
+    Ptr<TonemapMantiuk> tonemapMantiuk = createTonemapMantiuk(2.2f, 0.85f, 1.2f);
+    tonemapMantiuk->process(hdrDebevec, ldrMantiuk);
+    ldrMantiuk *= 3 * 255;
+    imwrite("ldr-Mantiuk.jpg", ldrMantiuk);
+    
+    // resizing and displaying all 4 tone mapped images
+    Size size(1024,768);
+    Mat im_ldrDrago    = imread("ldr-Drago.jpg");
+    Mat im_ldrDurand   = imread("ldr-Durand.jpg");
+    Mat im_ldrReinhard = imread("ldr-Reinhard.jpg");
+    Mat im_ldrMantiuk  = imread("ldr-Mantiuk.jpg");
+    
+    resize(im_ldrDrago,    im_ldrDrago,    size);
+    resize(im_ldrDurand,   im_ldrDurand,   size);
+    resize(im_ldrReinhard, im_ldrReinhard, size);
+    resize(im_ldrMantiuk,  im_ldrMantiuk,  size);
+    
+    imshow("LDR-Drago",    im_ldrDrago);
+    imshow("LDR-Durand",   im_ldrDurand);
+    imshow("LDR-Reinhard", im_ldrReinhard);
+    imshow("LDR-Mantiuk",  im_ldrMantiuk);
+    
+    // press any key to end the programm
+    waitKey(0);
 
 	return 0;
 }
 
 void read_images_and_times(vector<Mat> &images, vector<float> &times)
 {
-	int number_images = 1;
+	int number_images = 4;
 
 	// List of exposures times
 	static const float a_times[] = {1.0f/30.0f, 0.25f, 2.5f, 15.0f};
